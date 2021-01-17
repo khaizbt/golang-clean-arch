c90759b2b061afafefa2e45260b3be0347b4d523
Khaiz Badaru Tammam <tammam.dev@gmail.com>
2021-01-17 21:53:14 +0700

    [FIXED] TLS Config

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"goshop/middleware"
 	"goshop/repository"
 	"goshop/route"
 	"goshop/service"
 	"log"
+	"net/http"
 	"os"
 
 	sentrygin "github.com/getsentry/sentry-go/gin"
@@ -42,41 +44,40 @@ func main() {
 	router.Use(secureMiddleware)
 	router.Use(sentrygin.New(sentrygin.Options{}))
 	route.RouteUser(router, userService)
-	router.Run(":8000")
 
 	// Using This For Secure Request TLS
 
-	// certPair1, err := tls.LoadX509KeyPair("server.crt", "server.key")
-	// if err != nil {
-	// 	log.Fatalln("Failed to start web server", err)
-	// }
+	certPair1, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	if err != nil {
+		log.Fatalln("Failed to start web server", err)
+	}
 
-	// tlsConfig := new(tls.Config)
-	// tlsConfig.NextProtos = []string{"http/1.1"}
-	// tlsConfig.MinVersion = tls.VersionTLS12
-	// tlsConfig.PreferServerCipherSuites = true
+	tlsConfig := new(tls.Config)
+	tlsConfig.NextProtos = []string{"http/1.1"}
+	tlsConfig.MinVersion = tls.VersionTLS12
+	tlsConfig.PreferServerCipherSuites = true
 
-	// tlsConfig.Certificates = []tls.Certificate{
-	// 	certPair1, /** add other certificates here **/
-	// }
-	// tlsConfig.BuildNameToCertificate()
+	tlsConfig.Certificates = []tls.Certificate{
+		certPair1, /** add other certificates here **/
+	}
+	tlsConfig.BuildNameToCertificate()
 
-	// tlsConfig.ClientAuth = tls.VerifyClientCertIfGiven
-	// tlsConfig.CurvePreferences = []tls.CurveID{
-	// 	tls.CurveP521,
-	// 	tls.CurveP384,
-	// 	tls.CurveP256,
-	// }
-	// tlsConfig.CipherSuites = []uint16{
-	// 	tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-	// 	tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-	// }
+	tlsConfig.ClientAuth = tls.VerifyClientCertIfGiven
+	tlsConfig.CurvePreferences = []tls.CurveID{
+		tls.CurveP521,
+		tls.CurveP384,
+		tls.CurveP256,
+	}
+	tlsConfig.CipherSuites = []uint16{
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+	}
 
-	// serverTls := http.Server{
-	// 	Addr:      ":8000",
-	// 	Handler:   router,
-	// 	TLSConfig: tlsConfig,
-	// }
+	serverTls := http.Server{
+		Addr:      ":8000",
+		Handler:   router,
+		TLSConfig: tlsConfig,
+	}
 
-	// serverTls.ListenAndServeTLS("", "")
+	serverTls.ListenAndServeTLS("", "")
 }
